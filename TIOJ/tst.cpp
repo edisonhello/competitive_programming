eccf021ffce72ce719453b2ee12646ee25a2f7c6
edisonhello <edisonhello@hotmail.com>
2016-03-16 08:25:17 +0800

    1005 water AC

--- TIOJ/tst.cpp
+++ TIOJ/tst.cpp
@@ -3,9 +3,19 @@ using namespace std;
 int a[20][20];
 
 int main(){
-    for(int i=0;i<20;i++){
-        for(int j=0;j<20;j++){
-            cout<<a[i][j]<<" ";
+    int n, a, b, c, d, t;
+    a = b = 1, c = 2, d = 0;
+    scanf("%d", &n);
+    while(n){
+        if(n & 1){
+            t = c;
+            c = a * c + 5 * b * d >> 1;
+            d = a * d + b * t >> 1;
         }
+        t = a;
+        a = a * a + 5 * b * b >> 1;
+        b = t * b;
+        n >>= 1;
     }
+    printf("%d", d);
 }
