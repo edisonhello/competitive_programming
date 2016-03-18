313ce91d04266dd5d053ea1a7c2bdadb0afd41fb
edisonhello <edisonhello@hotmail.com>
2016-03-18 12:33:34 +0800

    10895 TLE 1884ing

---
+++ TIOJ/1884_3.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long
+
+
+using namespace std;
+
+inline ll rit(){
+    ll t=0;
+    char j;
+    do{j=getchar();}while((j<'0' || j>'9'));
+    do{
+        t = t*10+j-'0';
+        j=getchar();
+
+    }while((j>='0' && j<='9'));
+    return t;
+}
+
+int main(){
+    ll t=rit();
+    while(t--){
+        ll n=rit(),k=rit(),l=rit();
+        vector<ll> wait,fnt,bak,dis;
+        wait.resize(n+2);
+        fnt.resize(n+2);
+        bak.resize(n+2);
+        dis.resize(n+2);
+        for(int i=0;i<n;i++){
+            wait.at(i)=rit();
+        }
+
+        for(int i=0;i<n;i++){
+            dis.at(i) = min(wait.at(n) , l-wait.at(n));
+
+            if(i-k>=0)fnt.at(i) = fnt.at(i-k)+dis.at(i)*2;
+            else fnt.at(i) = dis.at(i)*2;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(i+k<=n)bak.at(i)=bak.at(i+k)+dis.at(i)*2;
+            else bak.at(i)=dis.at(i)*2;
+        }
+
+        ll findmid=0;
+        while(wait.at(findmid++)<=l/2) continue;
+
+        ll cas1,cas2 = 1000000000;
+        cas1 = fnt.at(findmid) + bak.at(n-findmid);
+        for(int i=0;i<n;i++){
+            cas2 = min(cas2, fnt.at(findmid-i)+bak.at(findmid+i-k));
+        }
+
+
+        ll ans=min(cas1,cas2);
+
+        printf("%I64d\n",ans);
+    }
+}
