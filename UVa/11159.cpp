ac06caab38c4444c44f9cca17b9c46fbc0b04709
edisonhello <edisonhello@hotmail.com>
2016-03-23 10:13:29 +0800

    11159 BS

---
+++ UVa/11159.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;scanf("%d",&t);
+    for(int i=1;i<=t;i++){
+        printf("Case %d: ",t);
+
+        int n;scanf("%d",&n);
+        int l1[n];
+        for(int i=0;i<n;i++){
+            scanf("%d",&l1[i]);
+        }
+        int m;scanf("%d",&m);
+        int l2[m];
+        for(int i=0;i<m;i++){
+            scanf("%d",&l2[i]);
+        }
+        bool line[n][m];
+        memset(line,0,sizeof(line));
+
+
+    }
+}
