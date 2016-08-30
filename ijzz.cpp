4abe546ce618f3e44d34aa924a9d35c779ea038a
edisonhello <edisonhello@hotmail.com>
2016-08-30 23:38:59 +0800

    417 both AC

--- ijzz.cpp
+++ ijzz.cpp
@@ -5,7 +5,7 @@ int main(){
 	string s;
 	while(cin>>s){
 		unsigned ans=0;
-		unsigned e=0,meow=26;
+		unsigned e=0,meow=1;
 		bool yn=1;
 		for(int i=0;i<s.size();i++){
 			if(i!=0&&(s[i]<=s[i-1])){
@@ -14,9 +14,10 @@ int main(){
 				break;
 			}
 			else{
-				ans=(ans*meow+s[i]-'a'+1)-e;
+				cout<<"ans="<<ans<<" meow="<<meow<<endl;
+				ans=(ans*meow+s[i]-'a'+1);
 				e++;
-				meow*=26-e;
+				meow*=26-(s[i]-'a'+1);
 			}
 		}
 		if(yn)cout<<ans<<endl;
