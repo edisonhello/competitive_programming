bfeeefae67e4f2828d02c3171b880796c825d928
edisonhello <edisonhello@hotmail.com>
2016-09-02 23:24:04 +0800

    debugging

--- TIOJ/1006.py
+++ TIOJ/1006.py
@@ -1,13 +1,13 @@
 import subprocess
 import random as r
-args = "./1006.exe"
-argss= "./1006_a.exe"
+args = "./1006"
+argss= "./1006_a"
 while 1:
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     pp= subprocess.Popen(argss,stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    a=r.randint(0,9999999999999999999999999999999999999999999999)
+    a=r.randint(0,9999999999999999999999999999999999999999999999999999)
     # a=
-    b=r.randint(1,99999999999999999999999999999999999999)
+    b=r.randint(1,9999999999999999999999999999999999)
     p.stdin.write(str(a)+'\n'+str(b)+'\n');
     pp.stdin.write(str(a)+'\n'+str(b)+'\n');
     # p.stdin.write('999088056764865\n980406\n');
