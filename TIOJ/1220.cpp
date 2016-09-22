596a0e1fd594eac1009b938b72effb14050a0902
edisonhello <edisonhello@hotmail.com>
2016-09-22 23:57:46 +0800

    1220WA79 1481WA10

--- TIOJ/1220.cpp
+++ TIOJ/1220.cpp
@@ -1,63 +1,107 @@
 #include<bits/stdc++.h>
 #define X first
 #define Y second
+#define PB(x) push_back(x)
+#define DE cout<<"[INFO] "
 using namespace std;
 
-int n,m,i,j,x,y;
-vector<pair<int,int> > v;
-bool app[100005];
-int djs[100005],sz[100005];
-set<int> s;
+int n,m,xxx,yyy;
+int cntDegrees[100005],minDeg,nminDeg;
+int djs[100005],djs_size[100005];
+vector<int> connected[100005];
+bitset<100005> notAppered;
+int cnt_sets;
+bitset<100005> gone;
 
-int F(int a){
-    if(djs[a]==0)return a;
-    else return djs[a]=F(djs[a]);
+int F(int x){
+    if(djs[x]==0) return x;
+    else return djs[x]=F(djs[x]);
 }
-void U(int a,int b){
-    if(a==b)return;
-    // cout<<"do U:"<<a<<" "<<b<<endl;
-    a=F(a),b=F(b);
-    if(a==b)return;
-    if(sz[a]>sz[b])swap(a,b);
-    djs[a]=b;
-    sz[b]+=sz[a];
+inline void U(int x,int y){
+    x=F(x),y=F(y);
+    if(x==y)return;
+    if(djs_size[x]>djs_size[y])swap(x,y);
+    djs_size[y]+=djs_size[x];
+    djs[x]=y;
     return;
+}
+inline bool S(int x,int y){
+    if(F(x)==F(y)) return 1;
+    else return 0;
+}
+
+int dfs(int now){
+    // DE<<"dfsing for now="<<now<<endl;
+    bitset<100005> tmpNotApp;
+    // memset(tmpNotApp,0,sizeof(tmpNotApp));
+    tmpNotApp[now]=1;
+    gone[now]=1;
+    for(int i=0;i<(int)connected[now].size();i++){
+        tmpNotApp[connected[now][i]]=1;
+    }
+    for(int i=1;i<=n;i++){
+        if(!tmpNotApp[i] && !gone[i]){
+            U(now,i);
+            int DFSResult = dfs(i);
+            if(DFSResult == -102928){
+                U(nminDeg,i);
+                return -102928;
+            }
+        }
+    }
+    return 39;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    // inputting
     cin>>n>>m;
-    for(i=0;i<m;i++){
-        cin>>x>>y;
-        v.push_back({x,y});
-        v.push_back({y,x});
+    for(int i=0;i<m;i++){
+        cin>>xxx>>yyy;
+        cntDegrees[xxx]++;
+        cntDegrees[yyy]++;
+        connected[xxx].PB(yyy);
+        connected[yyy].PB(xxx);
     }
-    sort(v.begin(),v.end());
-    memset(sz,1,sizeof(sz));
-    // for(i=0;i<v.size();i++)cout<<v[i].X<<" "<<v[i].Y<<endl;
-// cout<<"ed"<<endl;
-    for(i=0;i<v.size();i++){
-        // cout<<i<<endl;
-        if(i && v[i].X!=v[i-1].X){
-            for(j=1;j<=n;j++){
-                if(!app[j]){
-                    U(v[i-1].X,j);
-                }
-            }
-            memset(app,0,sizeof(app));
-        }
-        app[v[i].Y]=1;
-    }
-// cout<<"a4"<<endl;
-    for(j=1;j<=n;j++){
-        if(!app[j]){
-            U(v[i-1].X,j);
+    // inputting
+    // finding min deg
+    minDeg=cntDegrees[1];
+    nminDeg=1;
+    for(int i=2;i<=n;i++){
+        if(cntDegrees[i]<minDeg){
+            minDeg=cntDegrees[i];
+            nminDeg=i;
         }
     }
-    for(i=1;i<=n;i++){
-        s.insert(djs[i]);
-        // cout<<djs[i]<<" ";
+    memset(djs_size,1,sizeof(djs_size));
+    notAppered[nminDeg]=1;
+    for(int i=0;i<(int)connected[nminDeg].size();i++){
+        notAppered[connected[nminDeg][i]]=1;
     }
-    cout<<s.size()<<endl;
+    for(int i=1;i<=n;i++){
+        if(!notAppered[i]){
+            U(nminDeg,i);
+        }
+    }
+    cnt_sets=1;
+    // DE<<"get nminDeg="<<nminDeg<<"\n";
+    // DE<<"djs:";for(int i=1;i<=n;i++)cout<<djs[i]<<" ";cout<<endl;
+    // finding min deg
+    // djs
+    for(int i=1;i<=n;i++){
+        // DE<<"scanning "<<i<<"..."<<endl;
+        if(F(i)==F(nminDeg))continue;
+        int DFSResult = dfs(i);
+        if(DFSResult == -102928){
+            U(nminDeg,i);
+        }
+        else if(DFSResult == 39){
+            cnt_sets++;
+            U(nminDeg,i);
+        }
+        // DE<<"djs:";for(int i=1;i<=n;i++)cout<<djs[i]<<" ";cout<<endl;
+    }
+    // djs
+    cout<<cnt_sets<<endl;
 }
--- jizz.cpp
+++ jizz.cpp
@@ -1,47 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define PB(x) push_back(x)
+#define MP(x,y) make_pair(x,y)
+#define pii pair<int,int>
+#define vint vector<int>
+#define rz(x) resize(x)
+#define X first
+#define Y second
+// #define m (l+r)/2
+// #define xm (x1+x2)/2
+// #define ym (y1+y2)/2
+#define DE cout<<"de"<<endl;
+#define PQ priority_queue
 
-map<string,int> m;
-int cnt;
+ll tt,k;
+inline ll rit(){
+    tt=0,k=1;
+    char c;
+    do{
+        c=getchar();
+        if(c=='-')k=-1;
+    }while(c<'0'||c>'9');
+    do{
+        tt=tt*10+c-'0';
+        c=getchar();
+    }while(c>='0'&&c<='9');
+    return tt*k;
+};
 
-void spawn(string s,int mx){
-    if((int)s.length()>=mx){
-        m[s]=cnt;
-        cnt++;
-        return;
-    }
-    if(!s.length()){
-        for(int i='a';i<='z';i++){
-            spawn(s+(char)i,mx);
-        }
-        return;
-    }
-    for(int i=s[s.length()-1]+1;i<='z';i++){
-        spawn(s+(char)i,mx);
-    }
-}
-
-void init(){
-	cnt=1;
-    for(int i=1;i<=5;i++){
-        spawn("",i);
-    }
-}
 
 int main(){
-    init();
-    string s;
-    while(cin>>s){
-        int no=0;
-        for(int i=1;i<(int)s.length();i++){
-            if(s[i]<=s[i-1]){
-                cout<<0<<endl;
-                no=1;
-                break;
-            }
-        }
-        if(!no){
-            cout<<m[s]<<endl;
-        }
-    }
+    // ios_base::sync_with_stdio(0);
+    // cin.tie(0);
+    bitset<2000> b;
+    cout<<b<<endl;
 }
