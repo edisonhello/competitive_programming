0bbcf68b5e2f23473256a73f34de6f495e00314b
edisonhello <edisonhello@hotmail.com>
2016-10-16 20:24:43 +0800

    floyd warshell

---
+++ TIOJ/AC/12/1212.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define S(x) (int)(x).size()
+#define rz(x) reserve(x)
+#define PB(x) push_back(x)
+#define pii pair<int,int>
+#define vint vector<int>
+// #define m (l+r)/2
+// #define xm (x1+x2)/2
+// #define ym (y1+y2)/2
+#define DE cout<<"de"<<endl;
+#define SS stringstream
+#define PQ priority_queue
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+// #define getchar getchar_unlocked
+// #define putchar putchar_unlocked
+#define PVC(x) for(auto ___:(x))cout<<___<<" ";cout<<endl;
+#define PAR(x,n) for(int ___=0;___<(n);___++)cout<<x[___]<<" ";cout<<endl;
+
+char __c;bool __m;int __t;
+inline int rit();
+template<typename varType>
+inline bool rit(varType &inp);
+template<typename varType,typename ...Args>
+inline bool rit(varType &inp,Args &...args);
+
+int n,m,tott,d[505][505];
+
+int main(){
+    // cin.tie(0);
+    // ios_base::sync_with_stdio(0);
+    while(rit(n,m),n){
+        MS0(d);
+        tott=500000;
+        for(int i=0;i<m;++i){
+            int a=rit(),b=rit();
+            d[a][b]=1;
+        }
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                if(d[i][j]==0)d[i][j]=500000;
+                if(i==j)d[i][j]=0;
+            }
+        }
+        for(int k=1;k<=n;++k){
+            for(int i=1;i<=n;++i){
+                for(int j=1;j<=n;++j){
+                    if(i!=j){
+                        tott=min(tott,d[i][j]+d[j][i]);
+                    }
+                    d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+                }
+            }
+        }
+
+        if(tott==500000){
+            printf("0\n");
+        }
+        else{
+            printf("%d\n",tott);
+        }
+    }
+}
+
+inline int rit(){
+    __t=0,__m=0;
+    do{
+        __c=getchar();
+    }while((__c<'0'||__c>'9')&&(__c!='-'));
+    if(__c=='-')__m=1;
+    do{
+        __t=__t*10+__c-'0';
+        __c=getchar();
+    }while(__c>='0'&&__c<='9');
+    if(__m)__t=-__t;
+    return __t;
+}
+
+template<typename varType,typename ...Args>
+inline bool rit(varType &inp,Args &...args){
+    return rit(inp)&&rit(args...);
+}
+
+template<typename varType>
+inline bool rit(varType &inp){
+    inp=0;__m=0;
+    do{
+        __c=getchar();
+        if(__c==-1)return false;
+    }while((__c<'0'||__c>'9')&&__c!='-');
+    if(__c=='-')__m=1;
+    do{
+        inp=inp*10+__c-'0';
+        __c=getchar();
+    }while(__c>='0'&&__c<='9');
+    if(__m)inp=-inp;
+    return true;
+}
