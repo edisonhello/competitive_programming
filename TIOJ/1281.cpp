406dfd30af67e0ec32d6d5f432562e89ec51c6cf
edisonhello <edisonhello@hotmail.com>
2016-11-01 00:44:27 +0800

    1281 redo

--- TIOJ/1281.cpp
+++ TIOJ/1281.cpp
@@ -1,80 +1,32 @@
-#include<string>
 #include<iostream>
-#include<algorithm>
-#include<iomanip>
-#include<assert.h>
-#define int long long
-#define GC(a,b) (((a)+(b)<n?s[(a)+(b)]:2))
 using namespace std;
-struct _{int x,y,n,r;} a[100005];
-int n;string s;
+struct _{int r,x,y,n;} a[100005];
+bool cX(const _ &a,const _ &b){return a.x!=b.x?a.x<b.x:a.y<b.y;}
 bool cN(const _ &a,const _ &b){return a.n<b.n;}
-bool cR(const _ &a,const _ &b){return a.x!=b.x?a.x<b.x:a.y<b.y;}
-bool operator!=(const _ &a,const _ &b){return a.x!=b.x||a.y!=b.y;}
-
+int n;string s;
 void sa(){
-    sort(a,a+n,cR);
+    for(int i=0;i<n;++i)a[i].n=i,a[i].x=s[i];
+    sort(a,a+n,cX);
     a[0].r=1;
     for(int i=1,r=1;i<n;++i){
-        if(a[i]!=a[i-1])a[i].r=++r;
+        if(a[i].x>a[i-1].x||(a[i].x==a[i-1].x&&a[i].y<a[i-1].y))a[i].r=++r;
         else a[i].r=r;
     }
+    sort(a,a+n,cN);
     for(int i=0;i<n;++i){
         a[i].x=a[i].r;
     }
-    sort(a,a+n,cN);
     for(int D=1;D<=n;D<<=1){
-        for(int i=0;i<n;++i){
-            a[i].y=(i+D<n)?a[i+D].x:2;
-        }
-        // cout<<"tmpSA: ";for(int i=0;i<n;++i)cout<<"("<<a[i].x<<","<<a[i].y<<") ";cout<<endl;
-        sort(a,a+n,cR);
-        a[0].r=1;
-        for(int i=1,r=1;i<n;++i){
-            if(a[i]!=a[i-1])a[i].r=++r;
-            else a[i].r=r;
-        }
-        sort(a,a+n,cN);
+        
     }
-    sort(a,a+n,cR);
-    for(int i=0;i<n;++i){cout<<a[i].n<<endl;}
-    // cout<<"SA: \n";for(int i=0;i<n;++i){cout<<setw(2)<<a[i].n<<" ";for(int j=0;a[i].n+j<n;++j)cout<<s[a[i].n+j];cout<<endl;}
+
 }
-int pos[100005],lcpa[100005],lcpl,lcpx;
 void lcp(){
-    for(int i=0;i<n;++i){
-        pos[a[i].n]=i;
-    }
-    for(int i=0;i<n;++i,--lcpl){
-        if(lcpl<0)lcpl=0;
-        if(pos[i]==0){
-            lcpa[i]=lcpl;
-            continue;
-        }
-        while(a[pos[i]-1].n+lcpl>=n || i+lcpl>=n)--lcpl;
-        while(GC(a[pos[i]-1].n,lcpl)==GC(i,lcpl))++lcpl;
-        lcpa[i]=lcpl;
-        //cout<<"lcpa["<<i<<"]="<<lcpa[i]<<endl;
-        lcpx=max({lcpx,lcpl});
-    }
-    for(int i=0;i<n;++i){
-        if(lcpa[i]==lcpx){
-            for(int j=0;j<lcpx;++j){
-                cout<<s[i+j];
-            }
-            cout<<endl;
-            return;
-        }
-    }
+
 }
-signed main(){
-    ios_base::sync_with_stdio(0);
-    cin>>s;
+int main(){
+    getline(cin,s);
     n=(int)s.length();
-    for(int i=0;i<n;++i){
-        a[i].n=i;
-        a[i].x=GC(i,0);
-    }
     sa();
-    // lcp();
+    lcp();
 }
