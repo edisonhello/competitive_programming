ceb573591bd8563953bdfd2bc7293ead6d2c7dd7
edisonhello <edisonhello@hotmail.com>
2017-01-10 00:44:39 +0800

    give up BinaryTree

--- TIOJ/AC/14/1415.cpp
+++ TIOJ/AC/14/1415.cpp
@@ -1,32 +1,31 @@
-#include<iostream>
+#include<cstdio>
 #include<algorithm>
 #include<queue>
 using namespace std;
-#define int long long
 #define ll long long
 
 priority_queue<int,vector<int>,less<int>> pql;
 priority_queue<int,vector<int>,greater<int>> pqg;
 
 main(){
-int t;while(cin>>t){
+int t;while(~scanf("%d",&t)){
     ll ans=0;
     while(pql.size())pql.pop();
     while(pqg.size())pqg.pop();
 
     while(t--){
-        int n;cin>>n;
+        int n;scanf("%d",&n);
         while(n--){
-            int t;cin>>t;
+            int t;scanf("%d",&t);
             pql.push(t);
             pqg.push(t);
             // while((int)pql.size()>5005)pql.pop();
             // while((int)pqg.size()>5005)pqg.pop();
         }
         if(pql.size() && pqg.size()){
-            ans+=(int)(pql.top()-pqg.top());
+            ans+=(pql.top()-pqg.top());
             pql.pop(),pqg.pop();
         }
     }
-    cout<<ans<<endl;
+    printf("%lld\n",ans);
 }}
--- struct/BinaryTree.cpp
+++ struct/BinaryTree.cpp
@@ -33,22 +33,25 @@ public:
 template<typename T,typename cmp> // not use cmp
 class PQ{
 public:
-    void insert(T val){return this->_insert(root,val);}
+    PQ(){root=NULL;}
+    void insert(T val){this->_insert(root,val);}
     T top(){return this->_top(root);}
     T back(){return this->_back(root);}
     T pop_top(){return this->_pop_top(root);}
     T pop_back(){return this->_pop_back(root);}
-    void Pop_top(){return this->_Pop_top(root);}
-    void Pop_back(){return this->_Pop_back(root);}
+    void Pop_top(){this->_Pop_top(root);}
+    void Pop_back(){this->_Pop_back(root);}
     size_t size(){return root?root->sz:0;}
-    bool clear(){_clear(root);}
+    void clear(){_clear(root);}
     bool empty(){return root?1:0;}
 private:
     node<T> *root;
+    cmp Cmp;
     bool _insert(node<T> *&now,T val){
         if(!now)now = new node<T>(val);
-        else if(now->val<val)_insert(now->l,val);
+        else if(Cmp(val,now->val))_insert(now->l,val);
         else _insert(now->r,val);
+        // now->pull();
     }
     T _top(node<T> *now){
         while(now && now->r)now=now->r;
@@ -61,33 +64,55 @@ private:
     T _pop_top(node<T> *now){
         if(!now)return (T)0;
         node<T> *rc=now->r;
+        if(!rc){
+            root=now->l;
+            T rt=now->val;
+            delete now;
+            return rt;
+        }
         while(rc->r){now=rc;rc=rc->r;}
         T rt=rc->val;
-        now->r=NULL;
+        now->r=rc->l;
         delete rc;
         return rt;
     }
     T _pop_back(node<T> *now){
         if(!now)return (T)0;
         node<T> *lc=now->l;
+        if(!lc){
+            root=now->r;
+            T rt=now->val;
+            delete now;
+            return rt;
+        }
         while(lc->l){now=lc;lc=lc->l;}
         T rt=lc->val;
-        now->l=NULL;
+        now->l=lc->r;
         delete lc;
         return rt;
     }
     void _Pop_top(node<T> *now){
         if(!now)return (T)0;
         node<T> *rc=now->r;
+        if(!rc){
+            root=now->l;
+            delete now;
+            return;
+        }
         while(rc->r){now=rc;rc=rc->r;}
-        now->r=NULL;
+        now->r=rc->l;
         delete rc;
     }
     void _Pop_back(node<T> *now){
         if(!now)return (T)0;
         node<T> *lc=now->l;
+        if(!lc){
+            root=now->r;
+            delete now;
+            return;
+        }
         while(lc->l){now=lc;lc=lc->l;}
-        now->l=NULL;
+        now->l=lc->r;
         delete lc;
     }
     void clear(node<T> *now){
@@ -98,7 +123,16 @@ private:
 };
 
 #include<functional>
+#include<iostream>
 using namespace std;
 int main(){
     PQ<int,greater<int>> pq;
+    pq.insert(5);
+    pq.insert(7);
+    pq.insert(8);
+    pq.insert(3);
+    pq.insert(4);
+    pq.insert(2);
+    pq.insert(6);
+    cout<<pq.top()<<" "<<pq.pop_top()<<" "<<pq.pop_top()<<endl;
 }
