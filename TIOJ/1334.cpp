404f1f90b035add27c98390d8c09cb148c22610a
edisonhello <edisonhello@hotmail.com>
2017-02-18 08:17:30 +0800

    self practice day 4

---
+++ TIOJ/1334.cpp
@@ -0,0 +1,195 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define SZ(x) ((int)(x).size())
+#define LN(x) ((int)(x).length())
+#define rz(x) resize(x)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define endl '\n'
+#define el putchar('\n')
+#define spc putchar(' ')
+#define mid ((l+r)>>1)
+#define xm ((x1+x2)/2)
+#define ym ((y1+y2)/2)
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define CIO ios_base::sync_with_stdio(0);
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<'\n'
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<" , "<<#c<<" = "<<(c)<<'\n'
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<" , "<<#c<<" = "<<(c)<<" , "<<#d<<" = "<<(d)<<'\n'
+#define DEB(...) printf(__VA_ARGS__)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__)
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#endif
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto it:mp)ostm<<*it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto it:s)ostm<<*it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const priority_queue<T> &inp){priority_queue<T> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+
+inline int lowbit(int &x){return x&-x;}
+inline ll lowbit(ll &x){return x&-x;}
+template<typename T>
+inline T _pow(T b,int n){
+    T a=1; while(n){
+        if(n&1)a*=b; b*=b,n>>=1;
+    } return a;
+}
+template<typename T>
+inline T _pow(T b,int n,T mod){
+    T a=1%mod; while(n){
+        if(n&1)a=a*b%mod; b=b*b%mod,n>>=1;
+    } return a;
+}
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T& x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c-'0');
+    while(c=getchar(), c>='0' && c<='9')x=x*10+c-'0';
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-6;
+const ll mod=1e9+7;
+
+struct circ{ld x,y,r;} cs[3005];
+set<pair<ld,int>> rd[3005];
+
+inline ld dist(int i,int j){return sqrt((cs[i].x-cs[j].x)*(cs[i].x-cs[j].x)+(cs[i].y-cs[j].y)*(cs[i].y-cs[j].y));}
+
+int main(){
+    CIO; int n;cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>cs[i].x>>cs[i].y>>cs[i].r;
+        for(int j=0;j<i;++j){
+            ld distij=dist(i,j);
+
+            if(cs[i].r+cs[j].r-distij>eps){
+                {ld baseAngle=atan2(cs[j].y-cs[i].y,cs[j].x-cs[i].x);
+                if(baseAngle<0)baseAngle=2*PI-baseAngle;
+                ld cosTheta=acos((cs[i].r*cs[i].r+distij*distij-cs[j].r*cs[j].r)/(2*cs[i].r*distij));
+                if(baseAngle+cosTheta>2*PI){
+                    rd[i].insert({baseAngle-cosTheta,1});
+                    rd[i].insert({2*PI,-1});
+                    rd[i].insert({0,1});
+                    rd[i].insert({baseAngle+cosTheta-2*PI,-1});
+                }
+                else if(baseAngle-cosTheta<0){
+                    rd[i].insert({0,1});
+                    rd[i].insert({baseAngle+cosTheta,-1});
+                    rd[i].insert({baseAngle-cosTheta+2*PI,1});
+                    rd[i].insert({2*PI,-1});
+                }
+                else{
+                    rd[i].insert({baseAngle-cosTheta,1});
+                    rd[i].insert({baseAngle+cosTheta,-1});
+                }}{ld baseAngle=atan2(cs[i].y-cs[j].y,cs[i].x-cs[j].x);
+                if(baseAngle<0)baseAngle=2*PI-baseAngle;
+                ld cosTheta=acos((cs[j].r*cs[j].r+distij*distij-cs[i].r*cs[i].r)/(2*cs[j].r*distij));
+                PDE2(baseAngle,cosTheta);
+                if(baseAngle+cosTheta>2*PI){
+                    rd[j].insert({baseAngle-cosTheta,1});
+                    rd[j].insert({2*PI,-1});
+                    rd[j].insert({0,1});
+                    rd[j].insert({baseAngle+cosTheta-2*PI,-1});
+                }
+                else if(baseAngle-cosTheta<0){
+                    rd[j].insert({0,1});
+                    rd[j].insert({baseAngle+cosTheta,-1});
+                    rd[j].insert({baseAngle-cosTheta+2*PI,1});
+                    rd[j].insert({2*PI,-1});
+                }
+                else{
+                    rd[j].insert({baseAngle-cosTheta,1});
+                    rd[j].insert({baseAngle+cosTheta,-1});
+                }}
+            }
+        }
+    }
+    ld ans=0;
+    for(int i=0;i<n;++i){
+        int cnt=0; ld stp=0,cvr=0; for(auto pi:rd[i]){
+            PDE2(i,pi);
+            cnt+=pi.Y; if(cnt==1)stp=pi.X;
+            else if(cnt==0)cvr+=pi.X-stp;
+        }
+        ans+=(2*PI-cvr)*cs[i].r;
+    }
+    int tms=0;
+    while(ans-10.0>-eps){
+        ans/=10; ++tms;
+    }
+    cout<<fixed<<setprecision(4)<<ans<<" "<<tms<<endl;
+}
