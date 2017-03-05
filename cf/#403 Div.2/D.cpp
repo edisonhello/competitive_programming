caba72d704f437b9d3c406bf0ebfa2cccca45996
edisonhello <edisonhello@hotmail.com>
2017-03-06 00:43:53 +0800

    Codeforces #403 Rank:249 Rating 1702(+78)

---
+++ cf/#403 Div.2/D.cpp
@@ -0,0 +1,183 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define SZ(x) ((int)(x).size())
+#define LN(x) ((int)(x).length())
+#define rz(x) resize(x)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define endl '\n'
+#define el putchar('\n')
+#define spc putchar(' ')
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define CIO ios_base::sync_with_stdio(0);
+#define FLH fflush(stdout)
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<'\n'
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<" , "<<#c<<" = "<<(c)<<'\n'
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , "<<#b<<" = "<<(b)<<" , "<<#c<<" = "<<(c)<<" , "<<#d<<" = "<<(d)<<'\n'
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#ifdef WEA
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#else
+#define FIN ;
+#define FOUT ;
+#endif
+#endif
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto it:mp)ostm<<*it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto it:s)ostm<<*it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const priority_queue<T> &inp){priority_queue<T> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+
+inline int lowbit(int &x){return x&-x;}
+inline ll lowbit(ll &x){return x&-x;}
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T& x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c-'0');
+    while(c=getchar(), c>='0' && c<='9')x=x*10+c-'0';
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-8;
+const ll mod=1e9+7;
+
+int n,djs[3005];
+string nm[3005];
+vint grp[3005];
+int rec[3005];
+
+int F(int x){return x==djs[x]?x:djs[x]=F(djs[x]);}
+void U(int x,int y){djs[F(x)]=F(y);}
+
+bool dfs(set<string> &st,vint &v,int now){
+    if(now>=v.size())return 1;
+    if(st.find(nm[v[now]])==st.end()){
+        rec[now]=1;
+        st.insert(nm[v[now]]);
+        if(dfs(st,v,now+1))return 1;
+        st.erase(nm[v[now]]);
+    }
+    if(st.find(nm[v[now]+n])==st.end()){
+        rec[now]=2;
+        st.insert(nm[v[now]+n]);
+        if(dfs(st,v,now+1))return 1;
+        st.erase(nm[v[now]+n]);
+    }
+    return 0;
+}
+
+bool procG(int nm){
+    PDE1(nm);FLH;
+    vint &now=grp[nm];
+    set<string> app;
+    if(!dfs(app,now,0))return 0;
+    return 1;
+}
+
+int main(){
+    cin>>n;
+    for(int i=0;i<n;++i){
+        string s,t;cin>>s>>t;
+        nm[i]=s[0];nm[i]+=s[1];nm[i]+=s[2];
+        nm[i+n]=s[0];nm[i+n]+=s[1];nm[i+n]+=t[0];
+    }
+    for(int i=0;i<n;++i)djs[i]=i;
+    for(int i=0;i<2*n;++i){
+        for(int j=i+1;j<2*n;++j){
+            if(nm[i]==nm[j]){
+                i%=n,j%=n;
+                U(i,j);
+            }
+        }
+    }
+    for(int i=0;i<n;++i){
+        grp[F(i)].pb(i);
+    }
+    for(int i=0;i<n;++i){
+        if(!procG(i))return cout<<"NO\n",0;
+    }
+    cout<<"YES\n";
+    for(int i=0;i<n;++i){
+        if(rec[i]==1)cout<<nm[i]<<endl;
+        else cout<<nm[i+n]<<endl;
+    }
+}
+// 25262602 105:30 WA 5
