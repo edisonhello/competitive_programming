047a966bcaed58915996a40016c8e66ed3593cd2
edisonhello <edisonhello@hotmail.com>
2017-04-16 19:50:48 +0800

    zj water

---
+++ zj/AC/b/b981.cpp
@@ -0,0 +1,187 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define SZ(x) ((int)(x).size())
+#define LN(x) ((int)(x).length())
+#define rz(x) resize(x)
+#define reset(x,n) (x).clear(), (x).resize(n)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define el putchar('\n')
+#define spc putchar(' ')
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define CIO ios_base::sync_with_stdio(0);
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y2 Enyetuenwuevue
+#define left Ugbemugbem
+#define Osas
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG "jizz"
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define endl '\n'
+#define getchar gtx
+#define DEBUG 0
+#ifdef WEA
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#else
+#define FIN ;
+#define FOUT ;
+#endif
+#endif
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto &it:mp)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto &it:s)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const priority_queue<T> &inp){priority_queue<T> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+
+#define lowbit(x) ((x)&(-(x)))
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+void JIZZ(){cout<<"";exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-8;
+const ll mod=1e9+7;
+
+int main(){
+    string s;while(cin>>s){
+        int l=s.length();
+        if(s[l-1]=='r' && s[l-2]=='u' && s[l-3]=='o' && s[l-4]=='h'){ // 13hour
+            SS ss(s.substr(0,l-4));int H;ss>>H;
+            cout<<H*60*60*1000<<endl;
+        }
+        else if(s.find('h')!=string::npos && s.find('m')!=string::npos && s.find('s')==string::npos){ // 9h20m
+            int ph=s.find('h'),pm=s.find('m');
+            SS sh(s.substr(0,ph)),sm(s.substr(ph+1,l-pm-1-ph));
+            int H,M;sh>>H;sm>>M;
+            cout<<H*60*60*1000+M*60*1000<<endl;
+        }
+        else if(s.find('h')!=string::npos && s.find('m')!=string::npos && s.find('s')!=string::npos){ // 23h17m57s
+            int ph=s.find('h'),pm=s.find('m'),ps=s.find('s');
+            SS sh(s.substr(0,ph)),sm(s.substr(ph+1,pm-1-ph)),ss(s.substr(pm+1,l-pm-ph));
+            int H,M,S;sh>>H;sm>>M;ss>>S;
+            cout<<H*60*60*1000+M*60*1000+S*1000<<endl;
+        }
+        else if(s[l-1]=='n' && s[l-2]=='i' && s[l-3]=='m'){ // 6min
+            SS ss(s.substr(0,l-3));int M;ss>>M;
+            cout<<M*60*1000<<endl;
+        }
+        else if(s.find('m')!=string::npos && s.find('s')!=string::npos && s[l-2]!='m'){ // 34m50s
+            int pm=s.find('m'),ps=s.find('s');
+            SS sm(s.substr(0,pm)),ss(s.substr(pm+1,l-1-pm));
+            int M,S;sm>>M;ss>>S;
+            cout<<M*60*1000+S*1000<<endl;
+        }
+        else if(s[l-1]=='s' && s[l-2]=='m'){ // 567ms
+            int pm=s.find('m');
+            SS ss(s.substr(0,pm));
+            int ms;ss>>ms;
+            cout<<ms<<endl;
+        }
+        else if(s[l-1]=='s' && s.find('.')==string::npos){
+            int ps=s.find('s');
+            SS ss(s.substr(0,ps));
+            int S;ss>>S;
+            cout<<S*1000<<endl;
+        }
+        else{
+            int dot=s.find('.');
+            SS sint(s.substr(0,dot)),sfl(s.substr(dot+1,s.length()-2-dot));
+            int INT,FLO;sint>>INT;sfl>>FLO;
+            cout<<INT*1000+FLO*100<<endl;
+        }
+    }
+}
