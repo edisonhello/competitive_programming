68998acba887cd58c8cfe283d2cde6f9e4260d1e
edisonhello <edisonhello@hotmail.com>
2017-04-21 16:51:09 +0800

    no hack

---
+++ cf/contests/#410 Div.2/hack.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int len=s.length(),tot=0;
+    for(int i=0;i<len/2;++i){
+        if(s[i]!=s[len-1-i])++tot;
+    }
+    if(tot==1 || tot<=1 && len%2)cout<<"YES\n";
+    else cout<<"NO\n";
+}
