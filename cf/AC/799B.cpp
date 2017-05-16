72bb90162c55bcee0ecdf890ff1f45cea17150e1
edisonhello <edisonhello@hotmail.com>
2017-05-16 11:13:24 +0800

    meow

---
+++ cf/AC/799B.cpp
@@ -0,0 +1,152 @@
+// #include<cassert>
+#include<cstdio>
+// #include<cstdlib>
+// #include<cstring>
+// #include<cmath>
+// #include<ctime>
+#include<algorithm>
+// #include<iostream>
+// #include<iomanip>
+// #include<sstream>
+// #include<deque>
+// #include<queue>
+// #include<stack>
+// #include<map>
+// #include<set>
+// #include<unordered_map>
+// #include<unordered_set>
+// #include<bitset>
+// #include<vector>
+// #include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define SZ(x) ((int)(x).size())
+#define LN(x) ((int)(x).length())
+#define rz(x) resize(x)
+#define reset(x,n) (x).clear(), (x).resize(n)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define el putchar('\n')
+#define spc putchar(' ')
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define CIO ios_base::sync_with_stdio(0);
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y2 Enyetuenwuevue
+#define left Ugbemugbem
+#define Osas
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG "jizz"
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define endl '\n'
+#define getchar gtx
+#define DEBUG 0
+#ifdef WEA
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#else
+#define FIN ;
+#define FOUT ;
+#endif
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-8;
+const ll mod=1e9+7;
+
+int p[200009],ptr[5];
+vint c[5];
+bool u[200009];
+int main(){
+    int n;rit(n);
+    for(int i=1;i<=n;++i)rit(p[i]);
+    for(int i=1;i<=n;++i){
+        int fc;rit(fc);
+        c[fc].pb(i);
+    }
+    for(int i=1;i<=n;++i){
+        int bc;rit(bc);
+        c[bc].pb(i);
+    }
+    for(int i=1;i<=3;++i)sort(c[i].begin(),c[i].end(),[](const int &a,const int &b){return p[a]<p[b];});
+    int m;rit(m);
+    while(m--){
+        int cl;rit(cl);
+        while(ptr[cl]<c[cl].size() && u[c[cl][ptr[cl]]])++ptr[cl];
+        if(ptr[cl]>=c[cl].size())printf("-1 ");
+        else printf("%d ",p[c[cl][ptr[cl]]]),u[c[cl][ptr[cl]]]=1;
+    }
+    puts("");
+}
