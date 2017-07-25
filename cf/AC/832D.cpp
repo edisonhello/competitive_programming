1196bf4e6805c38bfa75628d7bd762903ffa6146
edisonhello <edisonhello@hotmail.com>
2017-07-25 19:51:47 +0800

    cf 832 jizzed.

---
+++ cf/AC/832D.cpp
@@ -0,0 +1,192 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<vector>
+#include<utility>
+#include<tuple>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define SZ(x) ((int)(x).size())
+#define LN(x) ((int)(x).length())
+#define rz(x) resize(x)
+#define reset(x,n) (x).clear(), (x).resize(n)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define el putchar('\n')
+#define spc putchar(' ')
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define Osas
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG "jizz"
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#define DEBUG 0
+#define CIO ios_base::sync_with_stdio(0),cin.tie(0);
+#ifdef WEA
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#else
+#define FIN ;
+#define FOUT ;
+#endif
+#endif
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto &it:mp)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto &it:s)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const priority_queue<T> &inp){priority_queue<T> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+
+#define lowbit(x) ((x)&(-(x)))
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+void JIZZ(){cout<<"";exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-8;
+const ll mod=1e9+7;
+
+vector<int> G[100005];
+int dep[100005], p[20][100005];
+
+int dfs(int now,int fa,int d){
+    dep[now]=d;
+    p[0][now]=fa;
+    for(int i=1;(1<<i)<=d;++i){
+        p[i][now]=p[i-1][p[i-1][now]];
+    }
+    for(int i:G[now]){
+        if(i==fa)continue;
+        dfs(i,now,d+1);
+    }
+}
+int LCA(int pa,int pb){
+    if(pa==pb)return pa;
+    if(dep[pb]>dep[pa])swap(pa,pb);
+    for(int i=0;i<20;++i){
+        if((dep[pa]-dep[pb])&(1<<i))pa=p[i][pa];
+        if(dep[pa]==dep[pb])break;
+    }
+    if(pa==pb)return pa;
+    for(int i=19;i>=0;--i){
+        if(p[i][pa]!=p[i][pb])pa=p[i][pa],pb=p[i][pb];
+        if(pa==pb)break;
+    }
+    if(pa==pb)return pa;
+    // if(p[0][pa]==0)exit(87);
+    return p[0][pa];
+}
+
+int gl(int a,int b){
+    int ab=LCA(a,b);
+    PDE3(a,b,ab);
+    if(ab==a || ab==b)return abs(dep[a]-dep[b]);
+    return dep[a]+dep[b]-2*dep[ab];
+}
+int l(int a,int b,int c){
+    PDE3(a,b,c);
+    return (gl(a,b)+gl(a,c)-gl(b,c))/2;
+}
+
+int main(){
+    int n,q; cin>>n>>q;
+    for(int i=2;i<=n;++i){
+        int t;cin>>t;
+        G[t].push_back(i); G[i].push_back(t);
+    }
+    dfs(1,0,0);
+    while(q--){
+        int a,b,c; cin>>a>>b>>c;
+        cout<<max({l(a,b,c),l(b,a,c),l(c,a,b)})+1<<endl;
+    }
+}
