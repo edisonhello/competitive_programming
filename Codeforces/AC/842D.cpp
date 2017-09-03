df8e390fb709a6fa2cae182bf16584ff25b48c2c
edisonhello <edisonhello@hotmail.com>
2017-09-03 16:50:24 +0800

    codeforces, 5/3 mo's algo TLE QQ

---
+++ Codeforces/AC/842D.cpp
@@ -0,0 +1,180 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define rz(x) resize(x)
+#define reset(x,n) (x).clear(),(x).resize(n)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define ws Osas
+#define dec tetteterette
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG 1
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#define FIN ;
+#define FOUT ;
+#define DEBUG 0
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+#if __cplusplus >= 201103L
+#include<unordered_map>
+#include<unordered_set>
+#include<tuple>
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto &it:mp)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto &it:s)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename TA,typename TB,typename TC> ostream& operator<<(ostream &ostm,const priority_queue<TA,TB,TC> &inp){priority_queue<TA,TB,TC> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+#endif
+
+void JIZZ(){cout<<"";exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-8;
+const ll mod=1e9+7;
+
+struct node{
+    node *ch[2];
+    int sz,tag;
+    node():sz(1),tag(0){ch[0]=ch[1]=0;};
+    int g0z(){return ch[0]?ch[0]->sz:0;}
+    int g1z(){return ch[1]?ch[1]->sz:0;}
+} *root;
+void insert(node *now,int val,int dep){
+    if(dep<0)return;
+    if(val&(1<<dep)){
+        PDE1("ins1");
+        if(!now->ch[1])now->ch[1]=new node();
+        insert(now->ch[1],val,dep-1);
+    }
+    else{
+        PDE1("ins0");
+        if(!now->ch[0])now->ch[0]=new node();
+        insert(now->ch[0],val,dep-1);
+    }
+    now->sz=now->g0z()+now->g1z();
+}
+int query(node *now,int dep){
+    if(!now)return 0;
+    PDE3(now,dep,now->tag&(1<<(dep)));
+    PDE2(now->g0z(),now->g1z());
+    if(now->tag&(1<<(dep)))swap(now->ch[0],now->ch[1]),now->tag^=(1<<dep);
+    if(now->ch[0])now->ch[0]->tag^=now->tag;
+    if(now->ch[1])now->ch[1]->tag^=now->tag;
+    now->tag=0;
+    PDE2(now->g0z(),now->g1z());
+    if(now->g0z()==0)return 0;
+    if(now->g0z()<(1<<dep))return query(now->ch[0],dep-1);
+    PDE1("go1");
+    return (1<<dep)+query(now->ch[1],dep-1);
+}
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n,m; cin>>n>>m; root=new node();
+    for(int i=0;i<n;++i){
+        int t; cin>>t;
+        insert(root,t,20);
+    }
+    while(m--){
+        int x; cin>>x; root->tag^=x;
+        cout<<query(root,20)<<endl;
+    }
+}
