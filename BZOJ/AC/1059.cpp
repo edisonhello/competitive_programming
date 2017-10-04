30bcf9b1bd6f42d6c9b29e01a5b4e67d53793353
edisonhello <edisonhello@hotmail.com>
2017-10-04 20:33:55 +0800

    BZOJ 1059: bigraph matching

--- BZOJ/AC/1059.cpp
+++ BZOJ/AC/1059.cpp
@@ -1,40 +1,151 @@
-#include<bits/stdc++.h>
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<utility>
 
 using namespace std;
 
-vector<int> G[444];
-int mch[444];
-bool v[444];
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define rz(x) resize(x)
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define FLH fflush(stdout)
 
-bool dfs(int now){
-    for(int i=0;i<G[now].size();++i){
-        if(v[G[now][i]])continue;
-        v[G[now][i]]=1;
-        if(mch[G[now][i]]==-1 || dfs(mch[G[now][i]])){
-            mch[G[now][i]]=now;
-            mch[now]=G[now][i];
-            return 1;
-        }
-    }
-    return 0;
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define ws Osas
+#define dec tetteterette
+#define exp expexpexpexp
+#define expl explexplexpl
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG 1
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#define FIN ;
+#define FOUT ;
+#define DEBUG 0
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+#if __cplusplus >= 201103L
+#include<unordered_map>
+#include<unordered_set>
+#include<tuple>
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
 }
 
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+#endif
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-13;
+const ll mod=1e9+7;
+
+vint G[555];
+int mat[555];
+bool u[555];
+bool dfs(int now){
+    // printf("dfs now %d\n",now);
+    if(u[now])return 0;
+    u[now]=1; for(int i=0;i<G[now].size();++i){
+        if(mat[G[now][i]]==-1 || dfs(mat[G[now][i]])){
+            mat[G[now][i]]=now;
+            mat[now]=G[now][i];
+            return 1;
+        }
+    } return 0;
+}
 int main(){
-    int ts;cin>>ts;while(ts--){
-        int n;cin>>n;
-        for(int i=0;i<=n*2+5;++i)G[i].clear();
-        for(int i=0;i<n;++i)for(int j=0;j<n;++j){
-            int t;cin>>t;if(t)G[i].push_back(j+n),G[j+n].push_back(i);
-        }
-        int m=0;
-        memset(mch,-1,sizeof(mch));
-        for(int i=0;i<n;++i){
-            if(mch[i]==-1){
-                memset(v,0,sizeof(v)); v[i]=1;
-                if(dfs(i))++m;
+    int ts; scanf("%d",&ts); while(ts--){
+        int n; scanf("%d",&n);
+        for(int i=0;i<555;++i)G[i].clear();
+        for(int i=0,t;i<n;++i)for(int j=0;j<n;++j){
+            scanf("%d",&t); if(t){
+                G[i].pb(j+n);
+                G[j+n].pb(i);
             }
-        }
-        if(m==n)cout<<"Yes\n";
-        else cout<<"No\n";
+        } MS(mat,-1); int cnt=0; 
+        for(int i=0;i<555;++i)if(mat[i]==-1){
+            if(MS0(u),dfs(i))++cnt;
+        } if(cnt==n)puts("Yes");
+        else puts("No");
     }
 }
