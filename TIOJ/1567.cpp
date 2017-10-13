d089ee08daf81589242b1c928383758cca370a2d
edisonhello <edisonhello@hotmail.com>
2017-10-13 21:00:10 +0800

    TIOJ 1576: implement

---
+++ TIOJ/1567.cpp
@@ -0,0 +1,174 @@
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define rz(x) resize(x)
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define ws Osas
+#define dec tetteterette
+#define exp expexpexpexp
+#define expl explexplexpl
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG 1
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#define FIN ;
+#define FOUT ;
+#define DEBUG 0
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+#if __cplusplus >= 201103L
+#include<unordered_map>
+#include<unordered_set>
+#include<tuple>
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto &it:mp)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto &it:s)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename TA,typename TB,typename TC> ostream& operator<<(ostream &ostm,const priority_queue<TA,TB,TC> &inp){priority_queue<TA,TB,TC> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+ostream& operator<<(ostream &ostm,const __int128 &val){__int128 cpy=val; stack<int> st; while(cpy)st.push(cpy%10),cpy/=10; while(st.size())ostm<<st.top(),st.pop();return ostm;}
+
+inline int gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+#endif
+
+void JIZZ(){cout<<"";exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-13;
+const ll mod=1e9+7;
+
+vector<pair<ld,ld>> pt;
+bitset<1000006> nc;
+ld p(ld x){return x*x;}
+ld dist(ld x1,ld y1,ld x2,ld y2){
+    return sqrt(p(x1-x2)+p(y1-y2));
+}
+int main(){
+    int n; ld d; cin>>n>>d;
+    for(int i=0;i<n;++i){
+        ld x,y; cin>>x>>y; pt.eb(x,y);
+    }
+    sort(pt.begin(),pt.end(),[](const pair<ld,ld> &a,const pair<ld,ld> &b){return a.X==b.X?abs(a.Y)>abs(b.Y):a.X<b.X;});
+    int cnt=0;
+    ld lx=-1e20;
+    for(int ii=0;ii<pt.size();++ii){
+        auto i=pt[ii];
+        if(dist(lx,0,i.X,i.Y)>d){
+            ++cnt;
+            nc[ii]=1;
+            if(abs(i.Y)>d){
+                cnt=-1;
+                break;
+            }
+            lx=i.X+sqrt(d*d-i.Y*i.Y);
+            for(int j=ii-1;j>=0;--j){
+                if(nc[j]){
+                    if(dist(lx,0,pt[j].X,pt[j].Y)<=d){
+                        nc[j]=0;
+                        --cnt;
+                    }
+                }
+                if(lx-pt[j].X>d)break;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+}
+
