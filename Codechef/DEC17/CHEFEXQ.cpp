2f683828f75ae68d6a2f226143a06b19fcda4fb2
edisonhello <edisonhello@hotmail.com>
2017-12-08 15:52:07 +0800

    Codechef now AC: 5, 535pts, rank 336

--- Codechef/DEC17/CHEFEXQ.cpp
+++ Codechef/DEC17/CHEFEXQ.cpp
@@ -1,5 +1,6 @@
 #include<cstdio>
-#pragma GCC optimize("Ofast")
+#include<map>
+#pragma GCC optimize("Ofast,no-stack-protector")
 #pragma comment(linker,"/STACK:36777216")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 using namespace std;
@@ -26,31 +27,64 @@ inline bool rit(T &x){
 template<typename T,typename ...Args>
 inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
  
-int a[1000006];
+struct node{
+    static node mem[10000000],*ptr;
+    node *l,*r;
+    int cnt;
+    node():l(0),r(0),cnt(0){}
+    node(node *ref):l(ref->l),r(ref->r),cnt(ref->cnt){}
+} *root[100005],node::mem[10000000],*node::ptr=node::mem,*rec;
+
+#define m ((l+r)>>1)
+void build(node *now,int l,int r){
+    if(l==r)return;
+    build(now->l=new (node::ptr++) node(),l,m);
+    build(now->r=new (node::ptr++) node(),m+1,r);
+}
+void mdf(node *now,int l,int r,int x){
+    if(l==x && x==r){++now->cnt; return;}
+    if(x<=m)mdf(now->l=new (node::ptr++) node(now->l),l,m,x);
+    else mdf(now->r=new (node::ptr++) node(now->r),m+1,r,x);
+}
+int qry(node *nowr,node *nowl,int l,int r,int x){
+    if(l==x && x==r)return nowr->cnt-nowl->cnt;
+    if(nowr==nowl)return 0;
+    if(x<=m)return qry(nowr->l,nowl->l,l,m,x);
+    else return qry(nowr->r,nowl->r,m+1,r,x);
+}
+
+int a[100005],p[100005],ch[100005];
 int main(){
     int n,q; rit(n,q);
-    for(int i=1;i<=n;++i)rit(a[i]);
-    int c,i,z;
+    build(root[0]=new (node::ptr++) node(),0,1048576);
+    rec=node::ptr;
+    for(int i=1;i<=n;++i)rit(a[i]),p[i]=p[i-1]^a[i],mdf(root[i]=new (node::ptr++) node(root[i-1]),0,1048576,p[i]);
+    map<int,int> mmap;
+    int c,i,z,mdfcnt=0;
     do{
         rit(c,i,z);
-        if(c==1)a[i]=z;
+        if(c==1){
+            mmap[i]=a[i]^z;
+            ch[i]=z;
+            ++mdfcnt;
+            if(mdfcnt>=500){
+                node::ptr=rec;
+                for(int i=1;i<=n;++i)(ch[i]?a[i]=ch[i],ch[i]=0:0),p[i]=p[i-1]^a[i],mdf(root[i]=new (node::ptr++) node(root[i-1]),0,1048576,p[i]);
+                mmap.clear();
+                mdfcnt=0;
+            }
+        }
         else{
-            int cnt=0,x=0,*p=a+1;
-            while(i&3){
-                x^=*p; --i; ++p;
-                cnt+=x==z;
+            int segstart=0,ans=0;
+            for(auto it:mmap){
+                if(it.first>i)break;
+                ans+=qry(root[it.first-1],root[segstart],0,1048576,z);
+                z^=it.second;
+                segstart=it.first-1;
             }
-            i>>=2;
-            while(i){
-                // cnt+=((x^=p[0])==z)+((x^=p[1])==z)+((x^=p[2])==z)+((x^=p[3])==z);
-                cnt+=((x^=p[0])==z);
-                cnt+=((x^=p[1])==z);
-                cnt+=((x^=p[2])==z);
-                cnt+=((x^=p[3])==z);
-                --i; p+=4;
-            }
-            printf("%d\n",cnt);
+            printf("%d\n",ans+qry(root[i],root[segstart],0,1048576,z));
         }
     }while(--q);
 }
 // TLE 350
+// AC 465
--- Codechef/DEC17/VK18.cpp
+++ Codechef/DEC17/VK18.cpp
@@ -12,21 +12,31 @@ int meow(int n){
     }
     return rec[nn]=abs(rec[nn]);
 }
+
+long long ans[2000006];
+void init(){
+    deque<int> dq; dq.push_back(meow(2)); 
+    long long sum=2; int ptr=2;
+    ans[1]=sum;
+    for(int i=2;i<=1000000;++i){
+        dq.push_back(meow(++ptr));
+        sum+=dq.back();
+        dq.push_back(meow(++ptr));
+        sum+=dq.back();
+        sum-=dq.front();
+        dq.pop_front();
+        ans[i]=ans[i-1]+sum*2-dq.back();
+    }
+}
  
 int main(){
     for(int i=0;i<2000006;++i)rec[i]=-1;
+    init();
     int t; cin>>t; while(t--){
-        int n,ptr=2,eptr=2; cin>>n;
-        ll sum=0,ans=0;
-        for(int i=0;i<n;++i,++ptr){
-            sum+=meow(ptr);
-        }
-        for(int i=0;i<n;++i,++ptr,++eptr){
-            ans+=sum;
-            sum+=meow(ptr)-meow(eptr);
-        }
-        cout<<ans<<endl;
+        int n; cin>>n;
+        cout<<ans[n]<<endl;
     }
 }
 // WA 350
 // TLE 380
+// AC 535
