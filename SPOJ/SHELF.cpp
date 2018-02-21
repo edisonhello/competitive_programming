0cb8f655c699ce72586bc04fcc234ac7c9aa92c8
edisonhello <edisonhello@hotmail.com>
2018-02-21 21:14:24 +0800

    nothing(?)

--- SPOJ/SHELF.cpp
+++ SPOJ/SHELF.cpp
@@ -6,13 +6,16 @@ ll dp[100005],h[100005],w[100005],wpre[100005];
 
 int main(){
     int n,l; cin>>n>>l;
-    set<ll> vals;
-    deque<int,int> deq;
+    multiset<ll> vals;
+    deque<int> deq;
     vals.insert(0);
-    deq.push_back({0,1000000009}); h[0]=1000000009;
+    deq.push_back(0); h[0]=1000000009;
     for(int i=1;i<=n;++i){
         cin>>h[i]>>w[i];
         wpre[i]=wpre[i-1]+w[i];
+        while(deq.size() && wpre[i]-wpre[deq.front()-1]>l){
+            auto it=vals.find(dp[
+        }
         while(h[deq.back()]<h[i]){
             // vals.erase(dp[deq.back()-1]+h[deq.back()]);
             vals.erase(
