5ca7152119ab463c9ec06da4c1c4182b16ac80fe
edisonhello <edisonhello@hotmail.com>
2018-05-31 11:21:42 +0800

    Add 2017 Taipei software compitition

---
+++ TPSC/2.cpp
@@ -0,0 +1,203 @@
+// #pragma GCC optimize("Ofast,no-stack-protector")
+
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<utility>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define rz(x) resize(x)
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vint vector<int>
+#define SS stringstream
+#define PQ priority_queue
+#define PRF(...) printf(__VA_ARGS__)
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define MS0(x) memset((x),0,sizeof(x))
+#define MSB(x) memset((x),0x3f,sizeof(x))
+#define MSBB(x) memset((x),0x7f,sizeof(x))
+#define MSM(x) memset((x),0xff,sizeof(x))
+#define MSMB(x) memset((x),0x80,sizeof(x))
+#define PAR(x,n) for(int ___=0;___<(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define PAR1(x,n) for(int ___=1;___<=(n);++___)cout<<x[___]<<" ";cout<<'\n';
+#define FLH fflush(stdout)
+
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define ws Osas
+#define dec tetteterette
+#define exp expexpexpexp
+#define expl explexplexpl
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#define PDE1(a) cout<<#a<<" = "<<(a)<<'\n'
+#define PDE2(a,b) cout<<#a<<" = "<<(a)<<" , ", PDE1(b)
+#define PDE3(a,b,c) cout<<#a<<" = "<<(a)<<" , ", PDE2(b,c)
+#define PDE4(a,b,c,d) cout<<#a<<" = "<<(a)<<" , ", PDE3(b,c,d)
+#define PDE5(a,b,c,d,e) cout<<#a<<" = "<<(a)<<" , ", PDE4(b,c,d,e)
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define FIN freopen("in","r",stdin)
+#define FOUT freopen("out","w",stdout)
+#define DEBUG 1
+#else
+#define PDE1(a) ;
+#define PDE2(a,b) ;
+#define PDE3(a,b,c) ;
+#define PDE4(a,b,c,d) ;
+#define PDE5(a,b,c,d,e) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define getchar gtx
+#define FIN ;
+#define FOUT ;
+#define DEBUG 0
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+#if __cplusplus >= 201103L
+#include<unordered_map>
+#include<unordered_set>
+#include<tuple>
+
+template<typename TA,typename TB> ostream& operator<<(ostream& ostm, const pair<TA,TB> &p){ostm<<"("<<p.X<<","<<p.Y<<")";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm, const vector<T> &v){ostm<<"[ ";for(auto i:v)ostm<<i<<" ";ostm<<"]";return ostm;}
+template<typename TA,typename TB> ostream& operator<<(ostream &ostm, const map<TA,TB> &mp){ostm<<"[ ";for(auto &it:mp)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const set<T> &s){ostm<<"[ ";for(auto &it:s)ostm<<it<<" ";ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const stack<T> &inp){stack<T> st=inp;ostm<<"[ ";while(!st.empty()){ostm<<st.top()<<" ";st.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const queue<T> &inp){queue<T> q=inp;ostm<<"[ ";while(!q.empty()){ostm<<q.front()<<" ";q.pop();}ostm<<"]";return ostm;}
+template<typename TA,typename TB,typename TC> ostream& operator<<(ostream &ostm,const priority_queue<TA,TB,TC> &inp){priority_queue<TA,TB,TC> pq=inp;ostm<<"[ ";while(!pq.empty()){ostm<<pq.top()<<" ";pq.pop();}ostm<<"]";return ostm;}
+template<typename T> ostream& operator<<(ostream &ostm,const deque<T> &inp){deque<T> dq=inp;ostm<<"[ ";while(!dq.empty()){ostm<<dq.front()<<" ";dq.pop_front();}ostm<<"]";return ostm;}
+// ostream& operator<<(ostream &ostm,const __int128 &val){__int128 cpy=val; stack<int> st; while(cpy)st.push(cpy%10),cpy/=10; while(st.size())ostm<<st.top(),st.pop();return ostm;}
+
+inline char gtx(){
+    const int N=1048576;
+    static char __buffer[N];
+    static char *__p=__buffer,*__end=__buffer;
+    if(__p==__end){
+        if((__end=__buffer+fread(__buffer,1,N,stdin))==__buffer)return EOF;
+        __p=__buffer;
+    } return *__p++;
+}
+
+template<typename T>
+inline bool rit(T &x){
+    char c=0; bool fg=0;
+    while(c=getchar(), (c<'0' && c!='-') || c>'9')if(c==EOF)return false;
+    c=='-' ? (fg=1,x=0) : (x=c&15);
+    while(c=getchar(), c>='0' && c<='9')x=x*10+(c&15);
+    if(fg)x=-x; return true;
+}
+template<typename T,typename ...Args>
+inline bool rit(T& x,Args& ...args){return rit(x)&&rit(args...);}
+
+inline void pit(int x){printf("%d",x);}
+inline void pln(ll x){printf("%I64d",x);}
+template<typename ...Args>
+inline void pit(int x,Args ...args){printf("%d ",x);pit(args...);}
+template<typename ...Args>
+inline void pln(ll x,Args ...args){printf("%I64d ",x);pit(args...);}
+#endif
+
+void JIZZ(){cout<<"";exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-13;
+const ll mod=1e9+7;
+
+
+template<int I>struct raw{
+    vector<ld> d;
+    raw(){d.resize(I);}
+    ld& operator[](int id){return d[id];}
+    const ld& operator[](int id)const{return d[id];}
+};
+template<int I>struct mat{
+    vector<raw<I>> r;
+    mat(){r.resize(I);}
+    raw<I>& operator[](int id){return r[id];}
+    const raw<I>& operator[](int id)const{return r[id];}
+    void setI(){for(int i=0;i<I;++i)for(int j=0;j<I;++j)r[i][j]=(i==j);}
+};
+template<int I>mat<I> operator*(const mat<I> &a,const mat<I> &b){
+    mat<I> rt;
+    for(int i=0;i<I;++i)for(int j=0;j<I;++j)for(int k=0;k<I;++k)rt[i][j]+=a[i][k]*b[k][j];
+    return rt;
+}
+template<int I>mat<I> operator+(const mat<I> &a,const mat<I> &b){
+    mat<I> rt;
+    for(int i=0;i<I;++i)for(int j=0;j<I;++j)rt[i][j]=a[i][j]+b[i][j];
+    return rt;
+}
+template<int I>ostream& operator<<(ostream &ostm,const raw<I> &r){ostm<<"["; for(int i=0;i<I;++i)ostm<<r[i]<<(i==I-1?"]":", "); return ostm;}
+template<int I>ostream& operator<<(ostream &ostm,const mat<I> &m){ostm<<"\n["; for(int i=0;i<I;++i)ostm<<m[i]<<(i==I-1?"]\n":",\n "); return ostm;}
+
+    mat<3> R,tk[3];
+void init(){
+    tk[0][0][0]=0.02; tk[0][0][1]=0.06; tk[0][0][2]=0.15;
+    tk[0][1][0]=0.02; tk[0][1][1]=0.04; tk[0][1][2]=0.18;
+    tk[0][2][0]=0.04; tk[0][2][1]=0.06; tk[0][2][2]=0.09;
+    tk[1][0][0]=0.16; tk[1][0][1]=0.03; tk[1][0][2]=0.20;
+    tk[1][1][0]=0.16; tk[1][1][1]=0.02; tk[1][1][2]=0.24;
+    tk[1][2][0]=0.32; tk[1][2][1]=0.03; tk[1][2][2]=0.12;
+    tk[2][0][0]=0.02; tk[2][0][1]=0.21; tk[2][0][2]=0.15;
+    tk[2][1][0]=0.02; tk[2][1][1]=0.14; tk[2][1][2]=0.18;
+    tk[2][2][0]=0.04; tk[2][2][1]=0.21; tk[2][2][2]=0.09;
+}
+vector<pair<ld,ll>> v;
+int main(){
+    init();
+    int ts; cin>>ts; while(ts--){
+        ll num; cin>>num;
+        string str; cin>>str;    
+        for(char &c:str)c-='0';
+        if(str[0]==0)R[0][0]=0.03, R[0][1]=0.06, R[0][2]=0.12;
+        if(str[0]==1)R[0][0]=0.24, R[0][1]=0.03, R[0][2]=0.28;
+        if(str[0]==2)R[0][0]=0.03, R[0][1]=0.21, R[0][2]=0.12;
+        for(int i=1;i<str.size();++i){
+            R=R*tk[str[i]];
+        }
+        ld p=0;
+        for(int i=0;i<3;++i)p+=R[0][i];
+        //  cout<<fixed<<setprecision(100)<<p<<endl;
+        v.emplace_back(p,num);
+    }
+    sort(v.begin(),v.end(),greater<pair<ld,ll>>());
+    vector<int> ans;
+    for(auto &i:v){
+        if(abs(v[0].first-i.first)/abs(v[0].first)<1e-10)ans.push_back(i.second);
+    }
+    sort(ans.begin(),ans.end());
+    for(int i:ans)cout<<i<<" "; cout<<endl;
+}
