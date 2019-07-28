308e06ccf6ed9201eba18ce87dbea57223cb569c
edisonhello <edisonhello@hotmail.com>
2019-07-29 00:25:18 +0800

    Codeforces Global Round 4
    Rank: 135, +13

---
+++ Codeforces/contests/Global Round 4/D.cpp
@@ -0,0 +1,133 @@
+// #pragma GCC optimize("no-stack-protector")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx,tune=native")
+// #pragma GCC diagnostic ignored "-W"
+
+#include<cassert>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<ctime>
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<vector>
+#include<utility>
+#include<functional>
+#include<complex>
+#include<climits>
+#include<random>
+#include<thread>
+
+#if __cplusplus >= 201103L
+#include<unordered_map>
+#include<unordered_set>
+#include<tuple>
+#endif
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// #include<ext/rope>
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int,int>
+#define vint vector<int>
+#define vpii vector<pair<int,int>>
+#define SS stringstream
+#define PQ priority_queue
+#define MS(x,v) memset((x),(v),sizeof(x))
+#define RZUNI(x) sort(x.begin(),x.end()), x.resize(unique(x.begin(),x.end())-x.begin())
+#define FLH fflush(stdout)
+#define CPPinput ios_base::sync_with_stdio(0); cin.tie(0)
+#define FIN(fname) freopen(fname,"r",stdin)
+#define FOUT(fname) freopen(fname,"w",stdout)
+
+#define tm Ovuvuevuevue
+#define y1 Enyetuenwuevue
+#define left Ugbemugbem
+#define ws Osas
+#define dec tetteterette
+#define expl explexplexpl
+#define data datadetedoto
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+
+#ifdef WEAK
+#include"/home/edison/Coding/cpp/template/debug.cpp"
+#define DEB(...) printf(__VA_ARGS__),fflush(stdout)
+#define WHR() printf("%s: Line %d",__PRETTY_FUNCTION__,__LINE__),fflush(stdout)
+#define LOG(...) printf("%s: Line %d ",__PRETTY_FUNCTION__,__LINE__),printf(__VA_ARGS__),fflush(stdout)
+#define DEBUG 1
+#define exit(x) cout<<"exit code "<<x<<endl, exit(0)
+#else
+#define PDE(...) ;
+#define DEB(...) ;
+#define WHR() ;
+#define LOG(...) ;
+#define DEBUG 0
+#endif
+
+#define lowbit(x) ((x)&(-(x)))
+
+void JIZZ(string output=""){cout<<output; exit(0);}
+
+const ld PI=3.14159265358979323846264338327950288;
+const ld eps=1e-10;
+const ll mod=1e9+7;
+
+bool np[2005];
+
+int main(){
+    CPPinput;
+    np[1] = 1;
+    for (int i = 2; i <= 1000; ++i) {
+        for (int j = i * i; j <= 2000; j += i) {
+            np[j] = 1;
+        }
+    }
+    int n; cin >> n;
+    if (n == 3) {
+        cout << "3" << endl;
+        cout << "1 2" << endl;
+        cout << "3 2" << endl;
+        cout << "1 3" << endl;
+        exit(0);
+    }
+    if (n == 4) {
+        cout << "5" << endl;
+        cout << "1 2\n1 3\n2 3\n2 4\n3 4\n";
+        exit(0);
+    }
+    vector<pair<int, int>> es;
+    for (int i = 1; i <= n; ++i) {
+        if (i == n) es.emplace_back(n, 1);
+        else es.emplace_back(i, i + 1);
+    }
+    int it = 1;
+    while (np[es.size()]) {
+        es.emplace_back(it, it + n / 2);
+        ++it;
+    }
+    cout << es.size() << endl;
+    for (auto &p : es) {
+        cout << p.first << " " << p.second << '\n';
+    }
+}
